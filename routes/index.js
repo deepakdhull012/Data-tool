2c2945483c8d653fa097a088b6e19506f8e59efc
Deepak Dhull <dpkdhull3@gmail.com>
2018-11-24 00:30:56 +0530

    Updated Code

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const textract = require('textract');
+const multer = require('multer');
 
-
-var textract = require('textract');
-var multer  = require('multer')
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
   }
 })
 
-var upload = multer({ storage: storage })
-var keyWords;
-extractKeys().then((res)=>{
+const upload = multer({ storage: storage });
+
+let keyWords;
+
+extractKeys().then((res) => {
   keyWords = res;
-  console.log(keyWords)
+}).catch((err) => {
+  console.log('Error in extraction: ', err);
 });
 
+function extractKeys() {
+  return new Promise((resolve, reject) => {
+    textract.fromFileWithPath('public\\uploads\\geoKeys.txt', function (error, keywordsFileContent) {
+      if (!error) {
+        resolve(JSON.parse(keywordsFileContent))
+      } else {
+        console.log(error);
+        reject(error);
+      }
+    });
+  });
+}
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('pages/index')
 });
 
-router.post('/', upload.single('fileUpload'),function(req, res) {
-  textract.fromFileWithPath(req.file.path, function( error, text ) {
-    if(!error){
-      var fileContent = text
+router.post('/', upload.single('fileUpload'), function (req, res) {
+  textract.fromFileWithPath(req.file.path, function (error, text) {
+    if (!error) {
+      let fileContent = text;
       res.json(processor(fileContent));
       res.end();
       fileContent = '';
-    }else {
-        console.log(error);
+    } else {
+      console.log(error);
     }
-})
+  })
 
 });
 
-let isQuestionAnswerSeperate = true;
+let isQuestionAnswerSeperate = true;   // Answers are at the bottom of file 
 
-let quesSep = 'Ques)'
+let quesSep = 'Ques.'
 let opAsep = '(a)'
 let opBsep = '(b)'
 let opCsep = '(c)'
@@ -52,160 +66,178 @@ let explanationSep = 'Explanation:)'
 
 
 
-function extractKeys(){
-return new Promise((resolve,reject) => {
-textract.fromFileWithPath('public\\uploads\\geoKeys.txt', function( error, text) {
-  if(!error){
-    resolve(JSON.parse(text))
-
-  }else {
-      console.log(error);
-      resolve(null)
-  }
-});
-});
-
-}
-
-
 let seggregatedQuestionList = []
 
 function getIndicesOf(searchStr, str, caseSensitive) {
-    var searchStrLen = searchStr.length;
-    if (searchStrLen == 0) {
-        return [];
-    }
-    var startIndex = 0, index, indices = [];
-    if (!caseSensitive) {
-        str = str.toLowerCase();
-        searchStr = searchStr.toLowerCase();
-    }
-    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
-        indices.push(index);
-        startIndex = index + searchStrLen;
-    }
-    return indices;
+  var searchStrLen = searchStr.length;
+  if (searchStrLen == 0) {
+    return [];
+  }
+  var startIndex = 0, index, indices = [];
+  if (!caseSensitive) {
+    str = str.toLowerCase();
+    searchStr = searchStr.toLowerCase();
+  }
+  while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+    indices.push(index);
+    startIndex = index + searchStrLen;
+  }
+  return indices;
 }
 
-function processor(fileContent){
+function getUnseggregatedQuestions(fileContent) {
+  let questionOccurences = getIndicesOf(quesSep, fileContent, false);
   let correctAnswerOccurence;
   let explanationOccurence;
-let questionOccurences = getIndicesOf(quesSep,fileContent);
-if(isQuestionAnswerSeperate){
-  correctAnswerOccurence = getIndicesOf(correctAnsSep,fileContent);
-  explanationOccurence = getIndicesOf(explanationSep,fileContent);
-}
 
-
-//console.log(questionOccurences)
-
-let unseggregatedQuestionList = []
-let correctAnswerList = []
-let explnationList = []
-for(let occurence in questionOccurences){
+  if (isQuestionAnswerSeperate) {
+    correctAnswerOccurence = getIndicesOf(correctAnsSep, fileContent);
+    explanationOccurence = getIndicesOf(explanationSep, fileContent);
+  }
+  let unseggregatedQuestionList = [];
+  let correctAnswerList = [];
+  let explnationList = [];
+  for (let occurence in questionOccurences) {
     let unseggregatedQuestion = ""
-    if(occurence == questionOccurences.length-1){
+    if (occurence == questionOccurences.length - 1) {
       let endPoint;
-      if(isQuestionAnswerSeperate){
+      if (isQuestionAnswerSeperate) {
         endPoint = fileContent.indexOf("ANSWERS & EXPLANATION");
       }
-      else{
+      else {
         endPoint = fileContent.length - 1;
       }
-    unseggregatedQuestion = fileContent.substr(questionOccurences[occurence],endPoint)
-  }
-    else{
-       unseggregatedQuestion = fileContent.substr(questionOccurences[occurence],questionOccurences[parseInt(occurence)+1]-1)
+      unseggregatedQuestion = fileContent.substr(questionOccurences[occurence], endPoint)
+    }
+    else {
+      unseggregatedQuestion = fileContent.substr(questionOccurences[occurence], questionOccurences[parseInt(occurence) + 1] - 1);
     }
     unseggregatedQuestionList.push(unseggregatedQuestion)
-}
+  }
 
-if(isQuestionAnswerSeperate){
-
-
-  for(let occurence of correctAnswerOccurence){
-      let correctAnswer = fileContent.substr(occurence,20);
+  if (isQuestionAnswerSeperate) {
+    for (let occurence of correctAnswerOccurence) {
+      let correctAnswer = fileContent.substr(occurence, 20);
       correctAnswerList.push(correctAnswer);
+    }
+    for (let occurence in explanationOccurence) {
+      let explanation = "";
+      if (occurence == explanationOccurence.length - 1) {
+        explanation = fileContent.substr(explanationOccurence[occurence], fileContent.length - 1);
+      }
+      else {
+        explanation = fileContent.substr(explanationOccurence[occurence], explanationOccurence[parseInt(occurence) + 1] - 10);
+      }
+      explnationList.push(explanation);
+    }
   }
-for(let occurence in explanationOccurence){
-  let explanation = "";
-  if(occurence == explanationOccurence.length-1){
-    explanation = fileContent.substr(explanationOccurence[occurence],fileContent.length-1);
-  }
-  else{
-    explanation = fileContent.substr(explanationOccurence[occurence],explanationOccurence[parseInt(occurence)+1]-10);
-  }
-  explnationList.push(explanation);
+
+  return {
+    "questions": unseggregatedQuestionList,
+    "correctAnswers": correctAnswerList,
+    "explanations": explnationList
+  };
 }
+
+function topicAssigner(qContent) {
+  let keyWordMatchdata = {
+    topicsMatched: {},
+    subjectsMatched: {}
+  };
+  for (let subject in keyWords) {
+    let noOfTimesSubjectMatched = 0;
+    let subjects = keyWords[subject];
+    for (let topic in subjects) {
+      let topics = subjects[topic];
+      noOfKeyWordMatched = 0;
+      for (let keyword of topics) {
+        if (qContent.toLowerCase().indexOf(keyword.toLowerCase()) >= 0) {
+          keyWordMatchdata.topicsMatched[topic] = ++noOfKeyWordMatched;
+          keyWordMatchdata.subjectsMatched[subject] = ++noOfTimesSubjectMatched;
+        }
+      }
+    }
+  }
+  if (Object.keys(keyWordMatchdata.topicsMatched).length > 0) {
+    return assignTopic(keyWordMatchdata);
+  } else {
+    return null;
+  }
+
+
+}
+function assignTopic(keyWordMatchData) {
+  console.log('KeyWord Match Data',keyWordMatchData);
+  let topicsMatched = [];
+  let subjectsMatched = [];
+  let maxKeywordsMatched = 0;
+  let noOfTimesSubjectMatchedMax = 0;
+  let topicMatchObject = keyWordMatchData.topicsMatched;
+  let subjectMatchObject = keyWordMatchData.subjectsMatched;
+  for (let topic in topicMatchObject) {
+
+    if (topicMatchObject[topic] === maxKeywordsMatched && maxKeywordsMatched > 0) {
+      topicsMatched.push(topic);
+    }
+    if (topicMatchObject[topic] > maxKeywordsMatched) {
+      // empty current List and push new
+      maxKeywordsMatched = topicMatchObject[topic];
+      topicsMatched = [];
+      topicsMatched.push(topic);
+    }
+    
+  }
+  for (let subject in subjectMatchObject) {
+
+    if (subjectMatchObject[subject] === noOfTimesSubjectMatchedMax && noOfTimesSubjectMatchedMax > 0) {
+      subjectsMatched.push(subject);
+    }
+    if (subjectMatchObject[subject] > noOfTimesSubjectMatchedMax) {
+      // empty current List and push new
+      noOfTimesSubjectMatchedMax = subjectMatchObject[subject];
+      subjectsMatched = [];
+      subjectsMatched.push(subject);
+    }
+    
+  }
+  return {
+    topicList: topicsMatched,
+    subjectList: subjectsMatched
+  }
 }
 
 
-console.log("No Of Questions: "+unseggregatedQuestionList.length)
-console.log("No Of Correct Answers: "+correctAnswerList.length)
-console.log("No Of Explanations: "+explnationList.length)
-seggregatedQuestionList = []
-for(let unseggregatedQuestion of unseggregatedQuestionList){
+
+function processor(fileContent) {
+  let unseggregatedQuestionData = getUnseggregatedQuestions(fileContent);
+  let unseggregatedQuestions = unseggregatedQuestionData.questions;
+  let correctAnswers = unseggregatedQuestionData.correctAnswers;
+  let explanations = unseggregatedQuestionData.explanations;
+  seggregatedQuestionList = []
+  for (let unseggregatedQuestion of unseggregatedQuestions) {
     let seggregatedQuestion = {}
-    seggregatedQuestion.qContent = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(quesSep)+quesSep.length,unseggregatedQuestion.indexOf(opAsep)-unseggregatedQuestion.indexOf(quesSep)-7).trim()
-    seggregatedQuestion.opA = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opAsep)+opAsep.length+1,unseggregatedQuestion.indexOf(opBsep)-unseggregatedQuestion.indexOf(opAsep)-4).trim()
-    seggregatedQuestion.opB = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opBsep)+opBsep.length+1,unseggregatedQuestion.indexOf(opCsep)-unseggregatedQuestion.indexOf(opBsep)-4).trim()
-    seggregatedQuestion.opC = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opCsep)+opCsep.length+1,unseggregatedQuestion.indexOf(opDsep)-unseggregatedQuestion.indexOf(opCsep)-4).trim()
-    seggregatedQuestion.opD = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opDsep)+opDsep.length+1,20).trim()
-    let noOfKeyWordMatchedVar = ""
-    let noOfKeyWordMatched = 0
-    seggregatedQuestion.keyWordMatchArray = []
-    seggregatedQuestion.keyWordMatchArray.km = []
-    for(let subject in keyWords){
-    for(let topic in keyWords[subject]){
-        noOfKeyWordMatched = 0
-        noOfKeyWordMatchedVar = topic+"matched"
-        let keyWordMatchdata = {}
-        keyWordMatchdata.km = []
-        for(let kw of keyWords[topic]){
-            if(seggregatedQuestion.qContent.toLowerCase().indexOf(kw.toLowerCase())>=0){
-                 ++noOfKeyWordMatched
-                 keyWordMatchdata.km.push(kw)
-            }
-        }
-
-
-        keyWordMatchdata[noOfKeyWordMatchedVar] = noOfKeyWordMatched
-        seggregatedQuestion.keyWordMatchArray.push(keyWordMatchdata)
+   // console.log((unseggregatedQuestion.indexOf(quesSep) + unseggregatedQuestion.length)-(unseggregatedQuestion.indexOf(opDsep) + opDsep.length));
+    seggregatedQuestion.qContent = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(quesSep) + quesSep.length, unseggregatedQuestion.indexOf(opAsep) - unseggregatedQuestion.indexOf(quesSep) - 6).trim();
+    seggregatedQuestion.opA = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opAsep) + opAsep.length, unseggregatedQuestion.indexOf(opBsep) - unseggregatedQuestion.indexOf(opAsep) - 4).trim();
+    seggregatedQuestion.opB = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opBsep) + opBsep.length, unseggregatedQuestion.indexOf(opCsep) - unseggregatedQuestion.indexOf(opBsep) - 4).trim();
+    seggregatedQuestion.opC = unseggregatedQuestion.substr(unseggregatedQuestion.indexOf(opCsep) + opCsep.length, unseggregatedQuestion.indexOf(opDsep) - unseggregatedQuestion.indexOf(opCsep) - 4).trim();
+    let opdStart = unseggregatedQuestion.indexOf(opDsep) + opDsep.length;
+    let strLen = unseggregatedQuestion.length - opdStart > 20 ? 20 : unseggregatedQuestion.length - opdStart;
+    seggregatedQuestion.opD = unseggregatedQuestion.substr(opdStart, strLen).trim();
+    const topicsAndSubject = topicAssigner(seggregatedQuestion.qContent);
+    if (topicsAndSubject) {
+      const topics = topicsAndSubject.topicList;
+      const subjects = topicsAndSubject.subjectList;
+      seggregatedQuestion.finalTopic = topics.length > 1 ? topics : topics[0];
+      seggregatedQuestion.subject = subjects.length > 1 ? subjects : subjects[0];
+    } else {
+      seggregatedQuestion.finalTopic = null;
+      seggregatedQuestion.subject = null;
     }
-  }
-
-    var noOfKeywordsMatched = 0
-    var max = 0
-    var topicName = ""
-    var finalTopicName = ""
-    for(let pair of seggregatedQuestion.keyWordMatchArray){
-        for(let key in pair){
-            noOfKeywordsMatched = pair[key]
-            topicName = key.replace("matched","")
-        }
-        if(noOfKeywordsMatched > max){
-            max = noOfKeywordsMatched
-            finalTopicName = topicName
-        }
-        else if(noOfKeywordsMatched == max){
-            finalTopicName = finalTopicName + "," +topicName
-        }
-    }
-    if(max == 0){
-        finalTopicName = "NA"
-    }
-    seggregatedQuestion.finalTopic = finalTopicName
-    delete seggregatedQuestion.keyWordMatchArray
-    //console.log(seggregatedQuestion)
-    //console.log("******************")
+    
     seggregatedQuestionList.push(seggregatedQuestion)
+  }
+  fileContent = "";
+  return seggregatedQuestionList;
 }
-fileContent = "";
-console.log(seggregatedQuestionList.length)
-return seggregatedQuestionList;
-
-}
-
-
-
 module.exports = router;
